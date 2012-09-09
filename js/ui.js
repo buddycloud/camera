5b8fd08bbd3abd2b74b045136fd085e975ddd589
Kristian <kristian.hartikainen@gmail.com>
2012-09-09 04:54:21 +0300

    Added flash effect on clicking the shutter button

---
+++ js/ui.js
@@ -0,0 +1,25 @@
+var transEndEventNames = {
+    'WebkitTransition' : 'webkitTransitionEnd',
+    'MozTransition'    : 'transitionend',
+    'OTransition'      : 'oTransitionEnd',
+    'msTransition'     : 'MSTransitionEnd',
+    'transition'       : 'transitionend'
+},
+transEndEventName = transEndEventNames[ Modernizr.prefixed('transition') ];
+
+var $shutter = $(".shutter");
+var triggerFlash = function() {
+  var $camera = $(".camera");
+  var $overlay = $(".flash");
+  overlay.on(transEndEventName, function() {
+    $camera.removeClass("action-shutterPressed");
+  });	
+  $camera.addClass("action-shutterPressed");
+  document.redraw();
+};
+
+var shutterClick = function() {
+  triggerFlash();
+};
+
+shutter.onclick = shutterClick;
